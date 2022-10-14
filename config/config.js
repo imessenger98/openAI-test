67176ccb3b89625dd6f973c73cc174cbd21d3460
MUHAMMED YAZEEN A N <muhammedyazeenan@duck.com>
2022-10-15 01:17:19 +0530

    created a basic call

--- config/config.js
+++ config/config.js
@@ -1,7 +1,8 @@
 import { Configuration, OpenAIApi } from "openai";
+import * as dotenv from 'dotenv' ;
+dotenv.config()
+
 const configuration = new Configuration({
-    organization: process.env.org_id,
     apiKey:process.env.secret_key,
 });
-const openai = new OpenAIApi(configuration);
-const response = await openai.listEngines();
+export const openAi = new OpenAIApi(configuration);
--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-const express = require('express')
-const app = express()
-require('dotenv').config();
-// console.log(process.env.PORT);
+import express from 'express';
+import * as dotenv from 'dotenv' ;
+import {openAi} from './config/config.js'
 
+dotenv.config()
+const app = express();
 app.get('/', function (req, res) {
   res.send('Hello World')
+})
+
+app.get('/areyouthere',async (req,res)=>{
+    try {
+        const completion = await openAi.createCompletion({
+          model: "text-davinci-001",
+          prompt: "are you there ?",
+        },
+        {
+            timeout: 20000,
+          }
+        );
+        res.send(completion.data.choices[0].text)
+      } catch (error) {
+        if (error.response) {
+          console.log(error.response.status);
+          console.log(error.response.data);
+        } else {
+          console.log(error.message);
+        }
+      }
+   
 })
 
 
